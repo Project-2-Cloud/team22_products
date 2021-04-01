08df46ad03d3d9f9db99b66a00d00ef6d0d59c5b
ArneArtois <43445014+ArneArtois@users.noreply.github.com>
2021-04-01 17:13:13 +0200

    Update ProductService.java

--- src/main/java/com/ibm/product/ProductService.java
+++ src/main/java/com/ibm/product/ProductService.java
@@ -30,26 +30,26 @@ public class ProductService {
         return Arrays.asList(
                 new Product(
                         "1",
-                        "macbook Retina 13.3' ME662 (2013)",
-                        "3.0GHz Dual-core Haswell Intel Core i5 Turbo Boost up to 3.2 GHz, 3MB L3 cache 8GB (two 4GB SO-DIMMs) of 1600MHz DDR3 SDRAM",
+                        "Free",
+                        "Free 1 month trial version for students",
                         "https://www.dropbox.com/s/swg9bdr0ejcbtrl/img9.jpg?raw=1",
-                        10,
+                        1000,
                         2399
                 ),
                 new Product(
                         "2",
-                        "Macbook Pro 13.3' Retina MF841LL/A",
-                        "Macbook Pro 13.3' Retina MF841LL/A Model 2015 Option Ram Care 12/2016",
+                        "Student service",
+                        "A 1 month licence for the student service, giving you acces to all content on the platform",
                         "https://www.dropbox.com/s/6tqcep7rk29l59e/img2.jpeg?raw=1",
-                        15,
+                        1000,
                         1199
                 ),
                 new Product(
                         "3",
-                        "Macbook Pro 15.4' Retina MC975LL/A Model 2012",
-                        "3.0GHz Dual-core Haswell Intel Core i5 Turbo Boost up to 3.2 GHz, 3MB L3 cache 8GB (two 4GB SO-DIMMs) of 1600MHz DDR3 SDRAM",
+                        "Enterprise",
+                        "A 1 month licence for the enterprise service, allowing you to upload and edit content on the platform. Access to all features and support forums.",
                         "https://www.dropbox.com/s/78fot6w894stu3n/img3.jpg?raw=1",
-                        1,
+                        1000,
                         1800
                 )
         );
