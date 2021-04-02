8b0fdab9392773fe9c393b0c9bcf4eef23b625d6
Jowan Abdo <48019820+jowan0@users.noreply.github.com>
2021-04-02 04:42:43 +0200

    Update ProductService.java

--- src/main/java/com/ibm/product/ProductService.java
+++ src/main/java/com/ibm/product/ProductService.java
@@ -26,31 +26,31 @@ public class ProductService {
 //            .forEach(topics::add);
 //        return topics;
 //    }
-    public List<Product> getAllProducts(){
+public List<Product> getAllProducts(){
         return Arrays.asList(
                 new Product(
                         "1",
-                        "macbook Retina 13.3' ME662 (2013)",
-                        "3.0GHz Dual-core Haswell Intel Core i5 Turbo Boost up to 3.2 GHz, 3MB L3 cache 8GB (two 4GB SO-DIMMs) of 1600MHz DDR3 SDRAM",
-                        "https://www.dropbox.com/s/swg9bdr0ejcbtrl/img9.jpg?raw=1",
-                        10,
-                        2399
+                        "Student software bundle",
+                        "All needed software for UCLL TI ",
+                        "https://i.imgur.com/JAHewcK.png",
+                        1000,
+                        29.99
                 ),
                 new Product(
                         "2",
-                        "Macbook Pro 13.3' Retina MF841LL/A",
-                        "Macbook Pro 13.3' Retina MF841LL/A Model 2015 Option Ram Care 12/2016",
-                        "https://www.dropbox.com/s/6tqcep7rk29l59e/img2.jpeg?raw=1",
-                        15,
-                        1199
+                        "Windows home license key",
+                        "Windows home x64 license",
+                        "https://i.imgur.com/FuvsOHa.png",
+                        1000,
+                        89.99
                 ),
                 new Product(
                         "3",
-                        "Macbook Pro 15.4' Retina MC975LL/A Model 2012",
-                        "3.0GHz Dual-core Haswell Intel Core i5 Turbo Boost up to 3.2 GHz, 3MB L3 cache 8GB (two 4GB SO-DIMMs) of 1600MHz DDR3 SDRAM",
-                        "https://www.dropbox.com/s/78fot6w894stu3n/img3.jpg?raw=1",
-                        1,
-                        1800
+                        "Windows pro license key",
+                        "Windows pro x64 license",
+                        "https://i.imgur.com/FuvsOHa.png",
+                        1000,
+                        109.99
                 )
         );
     }
